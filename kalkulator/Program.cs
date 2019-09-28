2a3a157bc44ff15f06ad3dc20c73c78c6c5db8d0
phoibosego <phoibosego@gmail.com>
2019-09-28 19:06:19 +0200

    Add menu to application

--- kalkulator/Program.cs
+++ kalkulator/Program.cs
@@ -10,6 +10,19 @@ namespace kalkulator
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Add");
+                Console.WriteLine("2. Diff");
+
+                int menuOption = Convert.ToInt32(Console.ReadLine());
+                if (menuOption == 0)
+                {
+                    break;
+                }
+            }
+
         }
     }
 }
