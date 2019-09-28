21e796993fa7bb7b2057a8788d48715a946f086e
phoibosego <phoibosego@gmail.com>
2019-09-28 19:33:08 +0200

    Menu reorganisation

--- kalkulator/Program.cs
+++ kalkulator/Program.cs
@@ -12,9 +12,14 @@ namespace kalkulator
         {
             while (true)
             {
-                Console.WriteLine("0. Exit");
-                Console.WriteLine("1. Add");
                 Console.WriteLine("2. Diff");
+                Console.WriteLine("0. Exit");
+
+                //jdfdfjsdf
+
+
+                Console.WriteLine("1. Add");
+               
 
                 int menuOption = Convert.ToInt32(Console.ReadLine());
                 if (menuOption == 0)
